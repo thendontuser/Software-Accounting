4589fabbd2e585900372a642e4e7ce8771d823aa
thendont <165030137+thendontuser@users.noreply.github.com>
2024-11-01 10:42:22 +0300

    Add files via upload

---
+++ Project/LogIn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Accounting_Client_
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void AutorizeBtn_Click(object sender, EventArgs e)
+        {
+            Autorization autorization = new Autorization();
+            autorization.Show();
+        }
+    }
+}
